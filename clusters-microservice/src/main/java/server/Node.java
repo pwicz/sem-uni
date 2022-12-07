c42b6f674c9d2be9044b06737833d0525195a17c
Timur Mukminov <T.I.Mukminov@student.tudelft.nl>
2022-12-07 21:03:21 +0100

    Added NodeRepository & Node class
    NodeRepository supports basic query like delete node,  get all nodes, get node by faculty
    
    Basic Node class intsantialiser and getters

---
+++ clusters-microservice/src/main/java/server/Node.java
@@ -0,0 +1,78 @@
+package server;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Node implements Comparable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Getter @Setter, does this work still?
+    private long id;
+    private String name; //netId?
+    private String url;
+    private String faculty;
+    private String token; //user auth token?
+    private int CPUaddtion;
+    private int GPUaddition;
+    private int memoryAddition;
+
+    public Node(String name, String url, String faculty, String token, int CPUaddtion, int GPUaddition, int memoryAddition) {
+        this.name = name;
+        this.url = url;
+        this.faculty = faculty;
+        this.token = token;
+        this.CPUaddtion = CPUaddtion;
+        this.GPUaddition = GPUaddition;
+        this.memoryAddition = memoryAddition;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public int getCPUaddtion() {
+        return CPUaddtion;
+    }
+
+    public int getGPUaddition() {
+        return GPUaddition;
+    }
+
+    public int getMemoryAddition() {
+        return memoryAddition;
+    }
+
+    /**
+     * Comparator for Node in descending order
+     * @param otherPlayer
+     */
+    @Override
+    public int compareTo(Object otherNode) {
+        int compareId = ((Node) otherNode).getId();
+        return compareId - this.id;
+    }
+
+}
---
+++ clusters-microservice/src/main/java/server/NodeRespository.java
@@ -0,0 +1,40 @@
+package server;
+
+import clusters-microservice.src.main.java.server.Node;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeRepository extends JpaRepository<Node, Long> {
+
+    /**
+     * Deletes the Node from the database
+     * @param id of the node you want to delete
+     * @return Optional of an Activity list
+     */
+    @Query(
+            nativeQuery=true,
+            value="DELETE FROM Node WHERE id = ?1")
+    void deletedNode(long id);
+
+    /**
+     * Gets all Nodes currently available
+     * Can only be done by an admin
+     * @param
+     * @return Optional of an Node list
+     */
+    @Query(
+            nativeQuery=true,
+            value="SELECT * FROM Node SORT BY faculty")
+    Optional<List<Node>> getAllNodes();
+
+    /**
+     * Gets all nodes belonging to the specific family
+     * @param the faculty you want to get the nodes of
+     * @return Optional of an Node list from the specific faculty
+     */
+    @Query(
+            nativeQuery=true,
+            value="SELECT * FROM Node WHERE faculty = ?1")
+    Optional<List<Node>> getNodesByFaculty(String faculty);
+
+
+}
