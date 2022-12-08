53996aa42cecc69d664a21f11bc3119dd1afe9f5
Axel Geist <A.O.Geist@student.tudelft.nl>
2022-12-08 16:20:03 +0100

    add Job class & commons setup

---
+++ commons/src/test/java/commons/JobTest.java
@@ -0,0 +1,52 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobTest {
+
+    @Test
+    void getJobId() {
+    }
+
+    @Test
+    void getNetId() {
+    }
+
+    @Test
+    void setNetId() {
+    }
+
+    @Test
+    void getResourceType() {
+    }
+
+    @Test
+    void setResourceType() {
+    }
+
+    @Test
+    void getCpuUsage() {
+    }
+
+    @Test
+    void setCpuUsage() {
+    }
+
+    @Test
+    void getGpuUsage() {
+    }
+
+    @Test
+    void setGpuUsage() {
+    }
+
+    @Test
+    void getMemoryUsage() {
+    }
+
+    @Test
+    void setMemoryUsage() {
+    }
+}
