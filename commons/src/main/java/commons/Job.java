b22e7d6905a0198e3b8a4664d8baa94effdd17e9
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-12-08 23:46:18 +0100

    Add @NoArgsConstructor to Job class

--- commons/src/main/java/commons/Job.java
+++ commons/src/main/java/commons/Job.java
@@ -1,11 +1,14 @@
 package commons;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 public class Job {
 
     @Id
