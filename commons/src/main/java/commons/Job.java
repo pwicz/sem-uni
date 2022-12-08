36ef00c933a1ec39a8737e54c626a8b73469bc7f
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-12-08 23:50:30 +0100

    Fix checkstyle :(

--- commons/src/main/java/commons/Job.java
+++ commons/src/main/java/commons/Job.java
@@ -1,11 +1,10 @@
 package commons;
 
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
