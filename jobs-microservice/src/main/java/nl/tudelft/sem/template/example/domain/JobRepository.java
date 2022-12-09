af3cf19250fe528fe697813c8d3869052914e454
mlica <M.Lica@student.tudelft.nl>
2022-12-09 18:20:27 +0100

    two query methods

--- jobs-microservice/src/main/java/nl/tudelft/sem/template/example/domain/JobRepository.java
+++ jobs-microservice/src/main/java/nl/tudelft/sem/template/example/domain/JobRepository.java
@@ -4,6 +4,24 @@ import nl.tudelft.sem.template.example.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+
+    /**
+     * Find all the jobs associated to a netID.
+     * @param netID netID of a user
+     * @return Optional<Job>
+     */
+    Optional<Job> findAllByNetID(NetID netID);
+
+    /**
+     * Find job by id.
+     * @param uuid id of the job
+     * @return Optional<Job>
+     */
+    Optional<Job> findByID(UUID uuid);
 }
