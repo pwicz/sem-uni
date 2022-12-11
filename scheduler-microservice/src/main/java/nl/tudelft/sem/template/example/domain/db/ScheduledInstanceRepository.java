62f19309c08153ef0a1a2ef8c21be0ea2ba84b1d
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-12-11 17:48:14 +0100

    Add findByDateAndFaculty repository method

--- scheduler-microservice/src/main/java/nl/tudelft/sem/template/example/domain/db/ScheduledInstanceRepository.java
+++ scheduler-microservice/src/main/java/nl/tudelft/sem/template/example/domain/db/ScheduledInstanceRepository.java
@@ -1,8 +1,11 @@
 package nl.tudelft.sem.template.example.domain.db;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduledInstanceRepository extends JpaRepository<ScheduledInstance, Long> {
     List<ScheduledInstance> findAllByJobId(long jobId);
+
+    List<ScheduledInstance> findByDateAndFaculty(LocalDate date, String faculty);
 }
