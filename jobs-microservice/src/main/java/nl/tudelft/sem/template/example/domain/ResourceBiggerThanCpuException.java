3b2e4b0adbfcf1214256b2038b8d1173354fbf68
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-12-17 21:06:52 +0100

    Add ResourceBiggerThanCpuException exception

---
+++ jobs-microservice/src/main/java/nl/tudelft/sem/template/example/domain/ResourceBiggerThanCpuException.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.template.example.domain;
+
+public class ResourceBiggerThanCpuException extends Exception {
+
+    static final long serialVersionUID = -3387516993124229948L;
+
+    public ResourceBiggerThanCpuException(String resourceOfProblem) {
+        super(resourceOfProblem + " usage cannot be greater than the CPU usage.");
+    }
+}
