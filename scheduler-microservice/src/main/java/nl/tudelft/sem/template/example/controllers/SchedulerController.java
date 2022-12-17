9d57e8cec70555e5bbd45d9725f094131350c330
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-12-17 21:32:27 +0100

    Add check for the CPU >= Max(GPU, Memory) resources requirement

--- scheduler-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/SchedulerController.java
+++ scheduler-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/SchedulerController.java
@@ -5,11 +5,13 @@ import commons.ScheduleJob;
 import nl.tudelft.sem.template.example.domain.processing.ProcessingJobsService;
 import nl.tudelft.sem.template.example.domain.processing.RemovingJobsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class SchedulerController {
@@ -32,7 +34,12 @@ public class SchedulerController {
      */
     @PostMapping("/schedule")
     public ResponseEntity<String> scheduleJob(@RequestBody ScheduleJob job) {
-        processingJobsService.scheduleJob(job);
+        try {
+            processingJobsService.scheduleJob(job);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+
         return ResponseEntity.ok("Processing");
     }
 
