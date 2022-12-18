de23982008565a78aa7e6374338dc2c0d859a675
mlica <M.Lica@student.tudelft.nl>
2022-12-18 13:40:08 +0100

    validator interface for jobs chain of responsability

---
+++ jobs-microservice/src/main/java/nl/tudelft/sem/template/example/chain/JobRejectedException.java
@@ -0,0 +1,8 @@
+package nl.tudelft.sem.template.example.chain;
+
+public class JobRejectedException extends Exception {
+
+    public JobRejectedException(String message) {
+        super(message);
+    }
+}
---
+++ jobs-microservice/src/main/java/nl/tudelft/sem/template/example/chain/Validator.java
@@ -0,0 +1,9 @@
+package nl.tudelft.sem.template.example.chain;
+
+import commons.Job;
+
+public interface Validator {
+
+    void setNext(Validator handler);
+    boolean handle(Job job) throws JobRejectedException;
+}
