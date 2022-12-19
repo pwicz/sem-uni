e58c96897435e2cb43bc232ef9605ed9ebbf2d24
Ilia Tomov <I.D.Tomov@student.tudelft.nl>
2022-12-19 14:11:53 +0100

    Refactor and add a test

--- clusters-microservice/src/main/java/nl/tudelft/sem/template/example/domain/FacultyCannotBeReleasedException.java
+++ clusters-microservice/src/main/java/nl/tudelft/sem/template/example/domain/FacultyCannotBeReleasedException.java
@@ -5,7 +5,7 @@ import nl.tudelft.sem.template.example.authentication.AuthManager;
 /**
  * Exception to indicate that an account is not authorized to release a faculty.
  */
-public class FacultyCannotBeReleasedException extends Throwable {
+public class FacultyCannotBeReleasedException extends Exception {
     static final long serialVersionUID = -3387516993124229948L;
 
     public FacultyCannotBeReleasedException(AuthManager authManager) {
--- clusters-microservice/src/main/java/nl/tudelft/sem/template/example/domain/UserNotInThisFacultyException.java
+++ clusters-microservice/src/main/java/nl/tudelft/sem/template/example/domain/UserNotInThisFacultyException.java
@@ -5,7 +5,7 @@ import nl.tudelft.sem.template.example.authentication.AuthManager;
 /**
  * Exception to indicate that an account is not authorized to release a faculty.
  */
-public class UserNotInThisFacultyException extends Throwable {
+public class UserNotInThisFacultyException extends Exception {
     static final long serialVersionUID = -3387516993124229948L;
 
     public UserNotInThisFacultyException(AuthManager authManager) {
