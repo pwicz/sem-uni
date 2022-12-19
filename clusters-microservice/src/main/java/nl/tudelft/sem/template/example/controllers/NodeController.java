39ceb0326aadc052abf3ef9794ba87004ec2e048
Ilia Tomov <I.D.Tomov@student.tudelft.nl>
2022-12-19 10:26:39 +0100

    fix

--- clusters-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/NodeController.java
+++ clusters-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/NodeController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
+@SuppressWarnings("PMD")
 @RestController
 @RequestMapping("/cluster")
 public class NodeController {
