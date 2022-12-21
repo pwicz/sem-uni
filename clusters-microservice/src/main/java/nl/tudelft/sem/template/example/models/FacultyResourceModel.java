348dd96fbfa2d8024577b448dbe286c7a103b49b
mlica <M.Lica@student.tudelft.nl>
2022-12-21 20:54:23 +0100

    refactor FacultyResourceModel error

--- clusters-microservice/src/main/java/nl/tudelft/sem/template/example/models/FacultyResourceModel.java
+++ clusters-microservice/src/main/java/nl/tudelft/sem/template/example/models/FacultyResourceModel.java
@@ -1,27 +1,10 @@
 package nl.tudelft.sem.template.example.models;
 
 import java.time.LocalDate;
+import lombok.Data;
 
+@Data
 public class FacultyResourceModel {
-    private transient String faculty;
-    private transient LocalDate date;
-
-    /**
-     * Constructs RequestModel for cluster.
-     *
-     * @param faculty faculty which resources belong to
-     * @param date date when the specified resources are available
-     */
-    public FacultyResourceModel(String faculty, LocalDate date) {
-        this.faculty = faculty;
-        this.date = date;
-    }
-
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
+    private String faculty;
+    private LocalDate date;
 }
