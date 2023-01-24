70b3f2d99f90af771e09d80fc2108729b3551291
mlica <M.Lica@student.tudelft.nl>
2023-01-24 10:24:32 +0100

    mutation testing for isAdmin method

--- commons/src/test/java/commons/RoleTest.java
+++ commons/src/test/java/commons/RoleTest.java
@@ -1,7 +1,10 @@
 package commons;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +37,18 @@ public class RoleTest {
     public void getRoleValueTest() {
         role = new Role(RoleValue.EMPLOYEE);
         assertNotNull(role.getRoleValue());
+    }
+
+    @Test
+    public void isAdminTest() {
+        role = new Role(RoleValue.ADMIN);
+        assertTrue(role.isAdmin());
+    }
+
+    @Test
+    public void isAdminTestNegate() {
+        role = new Role(RoleValue.EMPLOYEE);
+        assertFalse(role.isAdmin());
     }
 
 
