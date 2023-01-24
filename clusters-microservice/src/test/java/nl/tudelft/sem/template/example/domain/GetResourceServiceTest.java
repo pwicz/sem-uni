595d2524a03938d42ed7dbe54d8b075fdf7de019
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2023-01-24 16:36:22 +0100

    Add test that kills a manual mutant

--- clusters-microservice/src/test/java/nl/tudelft/sem/template/example/domain/GetResourceServiceTest.java
+++ clusters-microservice/src/test/java/nl/tudelft/sem/template/example/domain/GetResourceServiceTest.java
@@ -3,6 +3,8 @@ package nl.tudelft.sem.template.example.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import commons.FacultyResource;
+
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,21 @@ class GetResourceServiceTest {
         nodeRepository.saveAll(List.of(new Node("XYZ", "XYZ", "EEMCS2", "XYZ", 10, 10, 10),
                 new Node("XYZ2", "XYZ2", "EEMCS2", "XYZ2", 15, 2, 5),
                 new Node("XYZ3", "XYZ3", "3ME", "XYZ3", 10, 10, 10)));
+
+        var answer = getResourceService.getResourcesNextDay(List.of("EEMCS2"));
+        assertThat(answer.size()).isEqualTo(1);
+        assertThat(answer.stream().mapToInt(FacultyResource::getCpuUsage).sum()).isEqualTo(25);
+        assertThat(answer.stream().mapToInt(FacultyResource::getGpuUsage).sum()).isEqualTo(12);
+        assertThat(answer.stream().mapToInt(FacultyResource::getMemoryUsage).sum()).isEqualTo(15);
+    }
+
+    @Test
+    void getResourcesNextDay2() {
+        nodeRepository.saveAll(List.of(new Node("XYZ", "XYZ", "EEMCS2", "XYZ", 10, 10, 10),
+                new Node("XYZ2", "XYZ2", "EEMCS2", "XYZ2", 15, 2, 5),
+                new Node("XYZ3", "XYZ3", "3ME3", "XYZ3", 10, 10, 10)));
+
+        nodeRepository.updateRelease("3ME3", LocalDate.now().plusDays(2), LocalDate.now().plusDays(20));
 
         var answer = getResourceService.getResourcesNextDay(List.of("EEMCS2"));
         assertThat(answer.size()).isEqualTo(1);
