3afc2b409cc5a278b054bf7c153040f81c4187eb
Axel Geist <a.o.geist@student.tudelft.nl>
2023-01-25 16:24:21 +0100

    JobNotification mutation testing

--- jobs-microservice/src/test/java/nl/tudelft/sem/template/example/models/JobNotificationResponseModelTest.java
+++ jobs-microservice/src/test/java/nl/tudelft/sem/template/example/models/JobNotificationResponseModelTest.java
@@ -31,4 +31,69 @@ class JobNotificationResponseModelTest {
     void getScheduleDate() {
         assertThat(jnrm.getScheduleDate()).isEqualTo(LocalDate.now().plusDays(2));
     }
+
+    @Test
+    void setJobId() {
+        jnrm.setJobId(2L);
+        assertThat(jnrm.getJobId()).isEqualTo(2L);
+    }
+
+    @Test
+    void setStatus() {
+        jnrm.setStatus(Status.ACCEPTED);
+        assertThat(jnrm.getStatus()).isEqualTo(Status.ACCEPTED);
+    }
+
+    @Test
+    void setScheduleDate() {
+        jnrm.setScheduleDate(LocalDate.now().plusDays(1));
+        assertThat(jnrm.getScheduleDate()).isEqualTo(LocalDate.now().plusDays(1));
+    }
+
+    @Test
+    void toStringAccepted() {
+        JobNotificationResponseModel jnrm1 = new JobNotificationResponseModel(
+                1L, Status.ACCEPTED, LocalDate.now().plusDays(2));
+        String text = "Job with job id 1 is ACCEPTED. "
+                + "The Job will be executed on the "
+                + LocalDate.now().plusDays(2) + ".";
+        assertThat(jnrm1.toString()).isEqualTo(text);
+    }
+
+    @Test
+    void toStringFinished() {
+        JobNotificationResponseModel jnrm2 = new JobNotificationResponseModel(
+                1L, Status.FINISHED, LocalDate.now().plusDays(2));
+        String text = "Job with job id 1 is FINISHED. "
+                + "The Job was executed on the "
+                + LocalDate.now().plusDays(2) + ".";
+        assertThat(jnrm2.toString()).isEqualTo(text);
+    }
+
+    @Test
+    void toStringPending() {
+        JobNotificationResponseModel jnrm3 = new JobNotificationResponseModel(
+                1L, Status.PENDING, LocalDate.now().plusDays(2));
+        String text = "Job with job id 1 is PENDING. "
+                + "Please check at another time, if the Job has been scheduled!";
+        assertThat(jnrm3.toString()).isEqualTo(text);
+    }
+
+    @Test
+    void toStringRunning() {
+        JobNotificationResponseModel jnrm4 = new JobNotificationResponseModel(
+                1L, Status.RUNNING, LocalDate.now().plusDays(2));
+        String text = "Job with job id 1 is RUNNING. "
+                + "The Job is executed today.";
+        assertThat(jnrm4.toString()).isEqualTo(text);
+    }
+
+    @Test
+    void toStringRejected() {
+        JobNotificationResponseModel jnrm5 = new JobNotificationResponseModel(
+                1L, Status.REJECTED, LocalDate.now().plusDays(2));
+        String text = "Job with job id 1 is REJECTED. "
+                + "The Job is rejected, since no resources were available.";
+        assertThat(jnrm5.toString()).isEqualTo(text);
+    }
 }
